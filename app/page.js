8e6e13530abda9c74d6f8e445b0cdaaa6b970f08
Srijan703 <srijanpokhrel343@gmail.com>
2024-11-13 20:27:11 +0545

    SEO2

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ import Story from './components/Story';
 import MemeGallery from './components/MemeGallery';
 import Footer from './components/Footer';
 import Loading from './components/Loading';
-import UserPostedMeme from './components/UserPostedMeme';
+// import UserPostedMeme from './components/UserPostedMeme';
 
 export default function Home() {
   const [loadingComplete, setLoadingComplete] = useState(false);
@@ -22,14 +22,30 @@ export default function Home() {
       {loadingComplete && (
         <>
           <SEO 
-            title="RoboRuth 12: The AI Meme Revolution - Home" 
-            description="Discover the AI-powered meme journey with RoboRuth 12, exploring memes and more across the blockchain universe."
+            title="RoboRuth 12: The AI Meme Revolution | Blockchain's Heroine"
+            description="Meet RoboRuth 12, the AI-powered guardian of the meme-verse. Join her cosmic journey through the blockchain universe and be part of the meme revolution!"
+            url="https://roboruth12.com"
+            image="https://roboruth12-testing.vercel.app/image.png"
+            keywords={["AI Meme Revolution", "RoboRuth 12", "Blockchain Heroine", "Decentralized Finance", "Crypto Memes", "Meme Warriors"]}
+            og={{
+              title: "RoboRuth 12: The Meme Guardian of Blockchain",
+              type: "website",
+              url: "https://roboruth12.com",
+              image: "https://roboruth12-testing.vercel.app/image.png",
+              description: "Join RoboRuth 12 and her Meme Warriors in a cosmic journey through the blockchain universe to decentralize finance and unlock the power of AI.",
+              site_name: "RoboRuth 12",
+            }}
+            socialLinks={[
+              "https://x.com/RoboRuth12",
+              "https://t.me/RoboRuth12",
+            ]}
           />
+
           <Navbar />
           <Hero />
           <Story />
           <MemeGallery />
-          {/* <UserPostedMeme/> */}
+          {/* <UserPostedMeme /> */}
           <Footer />
         </>
       )}
